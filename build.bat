da8bb278caaa2615db144bc64bf7eaf565dfce0a
Oryxamus <greg@csprousers.org>
2026-09-02 15:57:28 -0400

    added yaml-cpp (version 0.8.0)

--- build.bat
+++ build.bat
@@ -12,6 +12,7 @@ copy /Y libwebm\LICENSE.TXT %LICENSES_DIR%\libwebm.txt
 copy /Y libwebp\COPYING %LICENSES_DIR%\libwebp.txt
 copy /Y md4c\LICENSE.md %LICENSES_DIR%\md4c.txt
 copy /Y pugixml\LICENSE.md %LICENSES_DIR%\pugixml.txt
+copy /Y yaml-cpp\LICENSE %LICENSES_DIR%\yaml-cpp.txt
 
 
 rem build the libraries
